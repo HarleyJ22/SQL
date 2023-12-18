17a3368ed42a5c17f07b9ff010eb459d3a007e46
Miriana Falcone <miriana.falcone7@gmail.com>
2023-12-18 09:15:42 +0100

    ex 1

---
+++ books-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Books (
+  book_id INTEGER PRIMARY KEY,
+  title VARCHAR(200),
+  author VARCHAR(200),
+  genre VARCHAR(200),
+  published_year INT,
+  isbn VARCHAR(13),
+  price DECIMAL(5, 2),
+  rating DECIMAL(2, 1),
+  stock_count INT
+  );
